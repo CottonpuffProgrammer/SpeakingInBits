57f92fccd57eb862a3f3fcf9f0286761fde6fb69
Isaac Detert <detert7392@students.cptc.edu>
2023-02-19 20:44:43 -0800

    All requirements on the assignment completed (all units tests done and logic implemented)

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -19,26 +19,73 @@ namespace CPW211_UnitTestStarterCode.Tests
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
             // Use the DataRow values to test the Add method
-            Assert.Fail();
+            double expectedResult = num1 + num2;
+
+            double returnedResult = SimpleMath.Add(num1, num2);
+
+            Assert.AreEqual(expectedResult, returnedResult);
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(3, 2)]
+        [DataRow(66, 7)]
+        [DataRow(-88, -9)]
+        [DataRow(0, -1)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
         {
             // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            double expectedResult = num1 * num2;
+
+            double returnedResult = SimpleMath.Multiply(num1, num2);
+
+            Assert.AreEqual(expectedResult, returnedResult);
         }
 
         [TestMethod]
-        public void Divide_DenominatorZero_ThrowsArgumentException()
+        [DataRow(58, 0)]
+        [DataRow(4, 0)]
+        [DataRow(-1000000, 0)]
+        [DataRow(32345, 0)]
+        public void Divide_DenominatorZero_ThrowsArgumentException(double num1, double num2)
         {
             // Divide by zero should throw an argument exception with a message
             // "Denominator cannot be zero"
-            Assert.Fail();
+            Assert.ThrowsException<ArgumentException>
+                (() => SimpleMath.Divide(num1, num2));
         }
 
-        // TODO: Test Divide method with two valid numbers
+        // The TODO asks for essentially the same test for two 
+        // different methods, looks slightly redundant but tests 
+        // what it needs to test
+
+        // TODO: Test Divide method with two valid 
+        [TestMethod]
+        [DataRow(3, 8888)]
+        [DataRow(5, 95)]
+        [DataRow(6, -80)]
+        [DataRow(4, -7)]
+        public void Divide_TwoNumbers_SuccessfulDivision(double num1, double num2)
+        {
+            double expectedResult = num1 / num2;
+
+            double returnedResult = SimpleMath.Divide(num1, num2);
+
+            Assert.AreEqual(expectedResult, returnedResult);
+        }
 
         // TODO: Test subtract method with two valid numbers
+        [TestMethod]
+        [DataRow(8, 6)]
+        [DataRow(-2, 7)]
+        [DataRow(15, -9)]
+        [DataRow(3, -107)]
+        public void Subtract_TwoNumbers_SuccessfulSubtraction(double num1, double num2)
+        {
+            double expectedResult = num1 - num2;
+
+            double returnedResult = SimpleMath.Subtract(num1, num2);
+
+            Assert.AreEqual(expectedResult, returnedResult);
+        }
     }
 }
